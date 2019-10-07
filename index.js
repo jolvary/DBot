80ec691fac92d04a66baa9209bec1f134b164cff
Server <unconfigured@null.spigotmc.org>
2019-10-07 21:37:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
+
 const prefix = '~';
 
 bot.on('message', message => {
