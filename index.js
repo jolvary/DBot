c466e718a3e373c16016c9121c47fb7eaaf240f0
Server <unconfigured@null.spigotmc.org>
2019-10-26 00:09:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ bot.on('message', message => {
 	if (message.content.includes('spawnea en 30 minutos')) {
 		message.delete (900100)
 		setTimeout(function() {
-			message.channel.send(chg[0]+'spawnea en 15 minutos <@&630800028084207631>');
+			message.channel.send(chg[0]+'spawnea en 15 minutos <@&630800028084207631>', {tts: true});
 		}, 900000);
 		
 	} else if (message.content.includes('15 minutos')) {
